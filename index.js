915feaa77adaff6891e028b1ff4624da3fcfe6b7
Lavanya Virushan <lavanya.virushan@gmail.com>
2023-04-03 12:56:30 -0400

    start app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config()
+const db = require('./config/connection');
+const routes = require("./routes");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    })
+});
